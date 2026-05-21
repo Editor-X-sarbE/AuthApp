b5da7083bd522ed9dd540ed9ad847a8140df2829
EditorXsarbE <routsarbeswar29@gmail.com>
2026-05-22 00:28:13 +0530

    Initial Spring Boot authentication project setup

---
+++ src/main/java/com/cutm/AuthApp/AuthAppApplication.java
@@ -0,0 +1,13 @@
+package com.cutm.AuthApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthAppApplication.class, args);
+		System.out.println("Project Works .");
+	}
+}
---
+++ src/test/java/com/cutm/AuthApp/AuthAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cutm.AuthApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
