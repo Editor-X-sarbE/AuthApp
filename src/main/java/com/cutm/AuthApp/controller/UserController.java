135a3638b4e0ae9ae503020d9d14dcd13e3f738c
EditorXsarbE <routsarbeswar29@gmail.com>
2026-06-01 22:28:45 +0530

    created all API

--- src/main/java/com/cutm/AuthApp/controller/UserController.java
+++ src/main/java/com/cutm/AuthApp/controller/UserController.java
@@ -8,6 +8,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -37,7 +40,28 @@ public class UserController {
     // Get User By Email API
     @GetMapping("/email/{email}")
     public ResponseEntity<UserDto> getUserByEmail(@PathVariable String email) {
-
         return ResponseEntity.ok(userService.getUserByEmail(email));
     }
+
+    // delete user
+    // api/v1/users/{userId}
+    @DeleteMapping("/{userId}")
+    public void deleteUser(@PathVariable("userId") String userId) {
+        userService.deleteUser(userId);
+    }
+
+    // update user
+    // api/v1/users/{userId}
+    @PutMapping("/{userId}")
+    public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto, @PathVariable("userId") String userId) {
+        return ResponseEntity.ok(userService.updateUser(userDto, userId));
+    }
+
+    // get user byId
+    // api/v1/users/{userId}
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserDto> getUserById(@PathVariable("userId") String userId) {
+        return ResponseEntity.ok(userService.getuserById(userId));
+    }
+
 }
--- src/main/java/com/cutm/AuthApp/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/cutm/AuthApp/exceptions/GlobalExceptionHandler.java
@@ -10,15 +10,29 @@ import com.cutm.AuthApp.dto.ErrorResponse;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleResourceNotFoundException(
-            ResourceNotFoundException exception) {
+        // resource not found handler method
+        @ExceptionHandler(ResourceNotFoundException.class)
+        public ResponseEntity<ErrorResponse> handleResourceNotFoundException(
+                        ResourceNotFoundException exception) {
 
-        ErrorResponse errorResponse = new ErrorResponse(
-                exception.getMessage(),
-                HttpStatus.NOT_FOUND);
+                ErrorResponse errorResponse = new ErrorResponse(
+                                exception.getMessage(),
+                                HttpStatus.NOT_FOUND, HttpStatus.NOT_FOUND.value());
 
-        return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(errorResponse);
-    }
+                return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                                .body(errorResponse);
+        }
+
+        // email not found handler method
+        @ExceptionHandler(IllegalArgumentException.class)
+        public ResponseEntity<ErrorResponse> handleIllegalArgumentException(
+                        IllegalArgumentException exception) {
+
+                ErrorResponse errorResponse = new ErrorResponse(
+                                exception.getMessage(),
+                                HttpStatus.BAD_REQUEST, HttpStatus.BAD_REQUEST.value());
+
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                                .body(errorResponse);
+        }
 }
