6c952238f233fae99cacf29b9ebf71f09bdcc141
EditorXsarbE <routsarbeswar29@gmail.com>
2026-06-03 22:18:56 +0530

    Change in Home Controller

--- src/main/java/com/cutm/AuthApp/AuthAppApplication.java
+++ src/main/java/com/cutm/AuthApp/AuthAppApplication.java
@@ -9,5 +9,6 @@ public class AuthAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AuthAppApplication.class, args);
 		System.out.println("Authentication Project Works .");
+		// Maintaining Streak
 	}
 }
