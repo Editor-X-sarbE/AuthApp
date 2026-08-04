029a78a7404670ddfcfa1445c4839c28ec6a96e4
EditorXsarbE <routsarbeswar29@gmail.com>
2026-08-04 12:33:52 +0530

    Password dont match (prob) .

--- src/main/java/com/cutm/AuthApp/config/SecurityConfig.java
+++ src/main/java/com/cutm/AuthApp/config/SecurityConfig.java
@@ -30,6 +30,7 @@ public class SecurityConfig {
                 authorizeHttpRequests -> authorizeHttpRequests
                         .requestMatchers("/api/v1/auth/register").permitAll()
                         .requestMatchers("/api/v1/auth/login").permitAll()
+                        .requestMatchers("/api/v1/users").permitAll()
                         .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults());
 
